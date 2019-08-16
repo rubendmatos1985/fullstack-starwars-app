26629c9d027d9c6d3af7ad385e2c893e9cf740b3
ruben1985 <rubendmatos1985@gmail.com>
2019-08-16 20:39:31 +0200

    seed for films

---
+++ seeds/02_films.ts
@@ -0,0 +1,19 @@
+import * as Knex from "knex";
+import getDataFromApi from '../src/utils/fetchData';
+import formatData from '../src/utils/formatData';
+export async function seed(knex: Knex): Promise<any> {
+    const d = await getDataFromApi('https://swapi.co/api/films/');
+    const data = formatData(d.results, [
+        "title",
+        "episode_id",
+        "opening_crawl",
+        "director",
+        "producer",
+        "release_date",
+        "created",
+        "edited",
+        "url"
+    ])
+    return knex("film").del()
+        .then(() => knex("film").insert(data));
+};
