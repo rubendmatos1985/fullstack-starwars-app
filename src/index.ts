4789fe873fd3778d65739af728fb30ae455a0fc4
ruben1985 <rubendmatos1985@gmail.com>
2019-08-16 21:06:23 +0200

    index server fixed

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,7 @@ const app = express();
 const knex = require("knex")(knexfile.development);
 
 app.get('/users', async (req:any, res:any)=>{
-  const r = await knex
-    .select('*')
-    .from('planet')
-    .where('id', '63d28a10-b908-11e9-b24b-2192b1684a4e')
-  
-  res.json(r);  
+
 })
 
 app.listen(3000, ()=> console.log("server started on port 3000"));
