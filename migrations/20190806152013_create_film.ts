53ed5c0f2a9a39a5db49c20c3ad71cd7648d6a37
ruben1985 <rubendmatos1985@gmail.com>
2019-08-16 20:32:53 +0200

    create films migration with types

--- migrations/20190806152013_create_film.ts
+++ migrations/20190806152013_create_film.ts
@@ -1,18 +1,19 @@
-exports.up = function(knex:any) {
-  return knex.schema.createTable('film', (table:any) =>{
-    table.uuid('id')
-    table.text('title')
-    table.integer('episode_id')
-    table.text('opening_crawl')
-    table.text('director')
-    table.text('producer')
-    table.text('release_date')
-    table.text('created')
-    table.text('edited')
-    table.text('url')  
-  })
+import Knex, { TableBuilder } from 'knex';
+exports.up = function(knex: Knex) {
+  return knex.schema.createTable("film", (table: TableBuilder) => {
+    table.uuid("id");
+    table.text("title");
+    table.integer("episode_id");
+    table.text("opening_crawl");
+    table.text("director");
+    table.text("producer");
+    table.text("release_date");
+    table.text("created");
+    table.text("edited");
+    table.text("url");
+  });
 
-
-exports.down = function(knex:any) {
-  return knex.schema.dropTable('film');
+  exports.down = function(knex: Knex) {
+    return knex.schema.dropTable("film");
+  };
 };
