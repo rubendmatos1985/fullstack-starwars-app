2505f4218c61d89cb6632e59ec4a01e9c7e6d9bd
ruben1985 <rubendmatos1985@gmail.com>
2019-08-26 18:26:04 +0200

    starship table created

---
+++ migrations/20190826181357_starship.ts
@@ -0,0 +1,28 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.createTable('starship', (t:Knex.TableBuilder)=>{
+      t.uuid('id').unique()
+      t.text('name')
+      t.text('model')
+      t.text('manufacturer')
+      t.text('cost_in_credits')
+      t.text('length')
+      t.text('max_atmosphering_speed')
+      t.text('crew')
+      t.text('passengers')
+      t.text('cargo_capacity')
+      t.text('consumables')
+      t.text('hyperdrive_rating')
+      t.text('MGLT')
+      t.text('starship_class')
+      t.text('url')
+  })
+}
+
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.dropTable('starship');
+}
+
