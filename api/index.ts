8f445980f52d8e614e043189345e9533752d87a0
ruben1985 <rubendmatos1985@gmail.com>
2019-08-27 16:11:00 +0200

    better types

--- api/index.ts
+++ api/index.ts
@@ -3,29 +3,27 @@ import { IPeopleFromApi } from "../models/People";
 import { IFilmFromApi } from "../models/Film";
 import { IApiResponse } from "./responseInterface";
 class Api {
-  static People: () => Promise<IPeopleFromApi[]> = async () => {
-    const result: IPeopleFromApi[] = await Promise.all(
+  static People: () => Promise<IPeopleFromApi[]> = async () =>
+    await Promise.all(
       Array(9)
         .fill(0)
         .map(
-          (_, i) =>
-            fetchData(`https://swapi.co/api/people/?page=${i + 1}`) as Promise<
-              IApiResponse
-            >
+          (_:number, i:number) =>
+            fetchData(`https://swapi.co/api/people/?page=${i + 1}`) as Promise<IApiResponse>
         )
-    ).then((response: IApiResponse[]) =>
-      response
-        .reduce((acc: any, obj: IApiResponse) => [...acc, obj.results], [] as IPeopleFromApi[])
-        .reduce(
-          (acc: IPeopleFromApi[], o: IPeopleFromApi[]) => [...acc, ...o],
+      )
+      .then((response: IApiResponse[]) =>
+        response.reduce(
+          (acc: any, obj: IApiResponse) =>
+            [...acc, ...obj.results] as IPeopleFromApi[],
           []
         )
-    );
-    return await result;
-  };
+      );
+
   static Film: () => Promise<IFilmFromApi[]> = async () =>
-    await fetchData("https://swapi.co/api/films/").then(
-      (response: IApiResponse) => response.results
-    );
+    await fetchData("https://swapi.co/api/films/")
+      .then(
+        (response: IApiResponse) => response.results
+      );
 }
 export default Api;
