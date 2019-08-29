f162276313334da8196971cddf62e72e848eb6bd
ruben1985 <rubendmatos1985@gmail.com>
2019-08-29 17:39:55 +0200

    unused import deleted

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import { Request, Response, Application } from 'express';
-import { IFilmEntity }from './interfaces/Film'
 const app:Application = require("express")();
 import Film from './models/Film';
 app.get('/films/:id', async (req:Request, res:Response)=>{
