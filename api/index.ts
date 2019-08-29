67b0ffa542687e678f3692093cadd06d3785ea4f
ruben1985 <rubendmatos1985@gmail.com>
2019-08-29 19:10:58 +0200

    Api Methods memoized

--- api/index.ts
+++ api/index.ts
@@ -1,44 +1,46 @@
-import fetchData from "../utils/fetchData";
-import { IPeopleFromApi } from "../interfaces/People";
-import { IFilmFromApi } from "../interfaces/Film";
+import fetchData from '../utils/fetchData';
+import { IPeopleFromApi } from '../interfaces/People';
+import { IFilmFromApi } from '../interfaces/Film';
 import { IPlanetFromApi } from '../interfaces/Planet';
-import { IApiResponse } from "./responseInterface";
-interface IApi{
-  People: ()=> Promise<IPeopleFromApi[]>
-  Film: ()=> Promise<IFilmFromApi[]>
-  Planet: ()=> Promise<IPlanetFromApi[]>
+import { IApiResponse } from './responseInterface';
+import { asyncMemoize as Mem } from '../utils/memoize';
+interface IApi {
+  People: Promise<any>;
+  Film: Promise<any>;
+  Planet: Promise<any>;
+  Starship: Promise<any>;
 }
-const Api:(()=> IApi) = ()=> ({
-    People:  async () =>
+const Api: IApi = {
+  People: Mem(
+    async (url: string) =>
       await Promise.all(
         Array(9)
           .fill(0)
-          .map(
-            (_:number, i:number) =>
-              fetchData(`https://swapi.co/api/people/?page=${i+1}`) as Promise<IApiResponse>
-          )
-        )
-        .then((response: IApiResponse[]) =>
-          response.reduce(
-            (acc: any, obj: IApiResponse) =>
-              [...acc, ...obj.results] as IPeopleFromApi[],
-            []
-          )
-        ),
-  
-    Film: async () =>(
-      await fetchData("https://swapi.co/api/films/")
-        .then(
-          (response: IApiResponse) => response.results
-        )
-    ),
-    Planet: async ()=>(
-      await Promise.all(
-        Array(7).fill(0)
-        .map((_, i)=> fetchData(`https://swapi.co/api/planets/?page=${i+1}`) as IApiResponse)
+          .map((_: number, i: number) => fetchData(url + i + 1) as Promise<IApiResponse>)
+      ).then((response: IApiResponse[]) =>
+        response.reduce((acc: any, obj: IApiResponse) => [...acc, ...obj.results] as IPeopleFromApi[], [])
       )
-      .then(v => v.reduce((acc:any, obj: IApiResponse)=> [...acc, ...obj.results] as IPlanetFromApi[],[]))
-        
-    )
-  })  
-export default Api();
+  )('https://swapi.co/api/people/?page='),
+
+  Film: Mem(async () => await fetchData('https://swapi.co/api/films/').then((response) => response.results))(
+    'https://swapi.co/api/films/'
+  ),
+
+  Planet: Mem(
+    async (url: string) =>
+      await Promise.all(
+        Array(7)
+          .fill(0)
+          .map((_, i) => fetchData(url + i + 1))
+      ).then((v) => v.reduce((acc: any, obj: IApiResponse) => [...acc, ...obj.results] as IPlanetFromApi[], []))
+  )('https://swapi.co/api/planets/?page='),
+  Starship: Mem(
+    async (url: string) =>
+      await Promise.all(
+        Array(4)
+          .fill(0)
+          .map((_, i) => fetchData(url + i + 1))
+      ).then((v) => v.reduce((acc: any, obj: IApiResponse) => [...acc, ...obj.results] as IPlanetFromApi[], []))
+  )('https://swapi.co/api/starships/?page=')
+};
+export default Api;
