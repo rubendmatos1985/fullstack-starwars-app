10914958464b3887b2ca812853be07efe3242bcd
ruben1985 <rubendmatos1985@gmail.com>
2019-09-02 17:58:44 +0200

    Method to fetch Specie from Api created

--- api/index.ts
+++ api/index.ts
@@ -7,14 +7,17 @@ import { asyncMemoize as Mem } from '../utils/memoize';
 import { ExecException } from 'child_process';
 import { IStarshipFromApi } from '../types/interfaces/Starship';
 import { IVehicleFromApi } from '../types/interfaces/Vehicle';
+import { ISpecieFromApi } from '../types/interfaces/Specie';
 interface IApi {
   People: () => Promise<IPeopleFromApi[]>;
   Film: () => Promise<IFilmFromApi[]>;
   Planet: () => Promise<IPlanetFromApi[]>;
   Starship: () => Promise<IStarshipFromApi[]>;
   Vehicle: ()=> Promise<IVehicleFromApi[]>
+  Specie: ()=> Promise<ISpecieFromApi[]>
 }
 const Api: IApi = {
+  
   People: Mem(
     async (url: string) =>
       await Promise.all(
@@ -34,6 +37,7 @@ const Api: IApi = {
         .then((response: any) => response.results)
         .catch((e: ExecException) => ({ message: 'error' }))
   )('https://swapi.co/api/films/'),
+  
   Planet: Mem(
     async (url: string) =>
       await Promise.all(
@@ -44,6 +48,7 @@ const Api: IApi = {
         .then((v) => v.reduce((acc: any, obj: any) => [...acc, ...(obj.results as any)], []))
         .catch((e: ExecException) => ({ message: 'error' }))
   )('https://swapi.co/api/planets/?page='),
+  
   Starship: Mem(
     async (url: string) =>
       await Promise.all(
@@ -54,6 +59,7 @@ const Api: IApi = {
         .then((v) => v.reduce((acc: any, obj: any) => [...acc, ...(obj.results as any[])], []))
         .catch((e: ExecException) => ({ message: 'error' }))
   )('https://swapi.co/api/starships/?page='),
+  
   Vehicle: Mem(
     async (url: string) =>
       await Promise.all(
@@ -63,7 +69,18 @@ const Api: IApi = {
       )
         .then((v) => v.reduce((acc: any, obj: any) => [...acc, ...(obj.results as any[])], []))
         .catch((e: ExecException) => ({ message: 'error' }))
-  )('https://swapi.co/api/vehicles/?page=')
+  )('https://swapi.co/api/vehicles/?page='),
+
+  Specie: Mem(
+   async (url:string)=>
+    await Promise.all(
+      Array(4)
+        .fill(0)
+        .map((_, i) => fetchData(`${url}${i + 1}`))
+    )
+      .then((v) => v.reduce((acc: any, obj: any) => [...acc, ...(obj.results as any[])], []))
+      .catch((e: ExecException) => ({ message: 'error' }))
+  )('https://swapi.co/api/species/?page=')
   
 };
 export default Api;
