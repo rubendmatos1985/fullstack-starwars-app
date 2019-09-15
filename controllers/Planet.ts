ed07365fd0bd50ba0fb5d186f9e9a47fe31d7c6d
ruben1985 <rubendmatos1985@gmail.com>
2019-09-15 16:05:59 +0200

    planet and film ready

--- controllers/Planet.ts
+++ controllers/Planet.ts
@@ -1,10 +1,14 @@
-import { IPlanet, IPlanetResponse } from "../types/interfaces/Planet";
+import { IPlanetResponse } from "../types/interfaces/Planet";
 import { asyncMemoize as Mem} from '../utils/memoize';
 import { getByIdQuery } from "../DB/queries";
 import { EntityTable, Table, ManyToManyTable } from "../types/Tables";
+import { knex } from "../DB";
+import { asyncCompose } from "../utils/asyncCompose";
 
 export default (()=>{
+  const getIds = (tableName:Table) => knex.select('id').from(tableName);
   const _getById = Mem(getByIdQuery<EntityTable.Planet, IPlanetResponse>(
+    
     EntityTable.Planet,
     [
       {
@@ -25,8 +29,14 @@ export default (()=>{
       }
     ]
   ))
+  const _getAll = (tables:{id: string}[])=> tables.map((t:{id:string})=> _getById(t.id)())
   return{
-    getById: _getById
+    getById: _getById,
+    getAll: async ()=> await asyncCompose(
+      (ps:Promise<IPlanetResponse>[])=> Promise.all(ps), 
+      _getAll, 
+      getIds
+      )(Promise.resolve(EntityTable.Planet))
   }
 })()
 
