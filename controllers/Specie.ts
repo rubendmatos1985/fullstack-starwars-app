2e154898f393ac963609eb8d3b63a09b257ff354
ruben1985 <rubendmatos1985@gmail.com>
2019-09-16 21:21:45 +0200

    routes created

---
+++ controllers/Specie.ts
@@ -0,0 +1,11 @@
+import { Router, Request, Response } from 'express';
+
+const R = Router();
+
+R.get('/:id', async (req:Request, res:Response)=>{
+    
+})
+
+R.get('/', async (req:Request, res: Response)=>{
+
+})
