5dc92ddbfadf089949adbd59618be7be01eede5b
ruben1985 <rubendmatos1985@gmail.com>
2019-09-16 21:16:28 +0200

    unused imports

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 import { Application, Request, Response } from 'express';
 import film from './routes/film';
-import Knex from 'knex';
 import helmet  from 'helmet';
 const app: Application = require('express')();
 import planet from './routes/planets';
 import people from './routes/people';
-
-app.use(helmet());
 app.use('/films', film);
 app.use('/planets', planet);
 app.use('/people', people);
 app.get('/test', (req: Request, res: Response) => {
   
 });
+
+app.use(helmet());
 app.listen(3000, () => console.log('server started on port 3000'));
 
