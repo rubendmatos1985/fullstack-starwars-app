4bdf3b986699258c8dc5b6d4fbb03c60a26bf57b
ruben1985 <rubendmatos1985@gmail.com>
2019-09-16 21:13:38 +0200

    helmet added

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import { Application, Request, Response } from 'express';
 import film from './routes/film';
 import Knex from 'knex';
+import helmet  from 'helmet';
 const app: Application = require('express')();
 import planet from './routes/planets';
 import people from './routes/people';
-const knex: Knex = require('knex')(require('./knexfile').development);
+
+app.use(helmet());
 app.use('/films', film);
 app.use('/planets', planet);
 app.use('/people', people);
