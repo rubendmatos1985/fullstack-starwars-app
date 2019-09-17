54d8807411ceb7d6bdaee8d7ce58d3c2ff1ad89b
ruben1985 <rubendmatos1985@gmail.com>
2019-09-17 10:41:47 +0200

    catch added to asyncCompose

--- utils/asyncCompose.ts
+++ utils/asyncCompose.ts
@@ -1,6 +1,10 @@
+import { ExecException } from "child_process";
+
 export const asyncCompose = (...fns: Function[]) => (data: Promise<any>) =>
   fns.reduceRight(
     (acc: Promise<any>, f: Function, index: number) =>
-      acc.then((v: any) => f(v))
+      acc
+        .then((v: any) => f(v))
+        .catch((e:ExecException) => console.log(e))
     , data
   )
