01c43f12b9d468a2e9a21b569ec026012f4ad935
ruben1985 <rubendmatos1985@gmail.com>
2019-09-22 22:23:46 +0200

    preparing app for server render client react

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = (props:any)=>(
+    <div>
+      Welcome To React!
+      <button onClick={ console.log }></button>
+    
+    </div>
+);
+
+
+export default App;
--- index.ts
+++ index.ts
@@ -1,24 +1,26 @@
-import { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import film from './routes/film';
 import helmet  from 'helmet';
 const app: Application = require('express')();
+import fs, { readFileSync } from 'fs';
+import path from 'path';
 import planet from './routes/planets';
 import people from './routes/people';
 import specie from './routes/species';
 import vehicle from './routes/vehicle';
 import starship from './routes/starship';
-import { matchRoutes } from 'react-router-config';
 type Port = string;
 const getPort = (p:NodeJS.Process):(Port | 3000) => p && p.env && p.env.PORT ? p.env.PORT : 3000;
+
+app.use(express.static('/client'))
+
 app.use('/films', film);
 app.use('/planets', planet);
 app.use('/people', people);
 app.use('/species', specie);
 app.use('/vehicles', vehicle)
 app.use('/starships', starship)
-app.get('/',(req:Request, res:Response)=>{
-    res.send('Ahhhhhhhhhhhhhhhhhhhh')
-})  
+app.get('/', (req:any, res:any) =>{})  
 app.use(helmet());
 app.listen(getPort(process), () => console.log(`server started on port ${getPort(process)}`));
 
