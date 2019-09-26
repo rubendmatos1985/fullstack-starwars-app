0ed24eafd92d3329f4157e2f43dfa184fb5970d3
ruben1985 <rubendmatos1985@gmail.com>
2019-09-26 17:33:30 +0200

    home and login started

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,19 @@
 import express from 'express'
 import { Application, Request, Response } from 'express';
 import helmet from 'helmet';
+import home from './routes/home';
 const app: Application = require('express')();
+import Joi from '@hapi/joi';
 import path from 'path';
 import getPort from './utils/port-getter';
 import apiRoot from './routes/api/root';
+
+app.use(helmet());
+app.use(helmet.hidePoweredBy());
+
 app.use('/static', express.static(path.join(path.dirname(__dirname), 'public')))
 app.use('/api/v1', apiRoot)
-app.use(helmet());
+app.use('/', home)
+
 app.listen(getPort(process), () => console.log(`server started on port ${getPort(process)}`));
 
