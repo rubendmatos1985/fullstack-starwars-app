942a5ab2bb1eac42a893934d15dee4e4d490e256
ruben1985 <rubendmatos1985@gmail.com>
2019-09-27 12:54:55 +0200

    user routes created

--- server/index.ts
+++ server/index.ts
@@ -6,16 +6,16 @@ import home from './routes/home';
 const app: Application = require('express')();
 import Joi from '@hapi/joi';
 import path from 'path';
+import user from './routes/user';
 import getPort from './utils/port-getter';
 import apiRoot from './routes/api/root';
 import cors from 'cors';
 app.use(helmet());
+app.use(express.json())
 app.use(cors({ origin: 'http://localhost:3001' }));
 app.use(helmet.hidePoweredBy());
-
-app.use('/static', express.static(path.join(path.dirname(__dirname), 'public')))
+app.use(express.static(path.join(path.dirname(__dirname), 'client', 'build')))
 app.use('/api/v1', apiRoot)
-app.use('/', home)
-
+app.use('/user', user)
 app.listen(getPort(process), () => console.log(`server started on port ${getPort(process)}`));
 
---
+++ server/routes/user/index.ts
@@ -0,0 +1,21 @@
+import Routes, { Router, Request, Response } from 'express';
+
+
+const R:Router = Routes();
+
+R.post('/signin',(req:Request, res: Response)=>{
+    console.log(req.body)
+    res.send('ok')
+})
+
+R.post('/login', (req: Request, res:Response)=>{
+   console.log(req.body)
+   res.send(req.body) 
+})
+
+R.post('/signout', (req:Request, res: Response)=>{
+    console.log(req.body)
+    res.send(req.body)
+})
+
+export default R;
