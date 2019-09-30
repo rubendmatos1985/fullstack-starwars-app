ab6d77dea8fd4edbfed8d326caf68d458b7dec39
ruben1985 <rubendmatos1985@gmail.com>
2019-09-30 10:45:10 +0200

    changing express-session for jwt

--- server/index.ts
+++ server/index.ts
@@ -1,42 +1,17 @@
 //import regenerator from 'regenerator-runtime/runtime';
 import express from 'express'
-import { Application, Request, Response } from 'express';
 import helmet from 'helmet';
-const app: Application = require('express')();
-import path from 'path';
+import Express, { Application } from 'express';
 import user from './routes/user';
 import getPort from './utils/port-getter';
 import apiRoot from './routes/api/root';
-import session from 'express-session';
-import settings from './settings';
-import connectPgSimple, { PGStore, PGStoreOptions } from 'connect-pg-simple';
-import { Pool } from 'pg';
-import uuid from 'uuid/v1';
-const pgPool: Pool = new Pool({
-  host: settings.host,
-  password: settings.password,
-  user: settings.user,
-  database: "starwars",
-  port: 5432,
-  ssl: true
-});
-const pgSession = connectPgSimple(session);
+
+const app:Application = Express();
+
 app.use(helmet())
 app.use(express.json())
 
-app.use(session({
-  store: new pgSession({
-    pool: pgPool,
-    tableName: 'user_session'
-  }),
-  secret: uuid(),
-  resave: false,
-  cookie: { 
-    maxAge: 1000 * 60 * 60,
-    httpOnly: true 
-  },
-  saveUninitialized: true
-}))
+
 app.use('/api/v1', apiRoot)
 app.use('/user', user)
 app.listen(getPort(process), () => console.log(`server started on port ${getPort(process)}`));
