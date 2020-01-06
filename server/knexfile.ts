15779ba169dee88789dcf2aeb32d6ce134662a8b
ruben1985 <rubendmatos1985@gmail.com>
2020-01-06 15:23:16 +0100

    port in knexfile fixed

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -3,7 +3,7 @@ module.exports = {
     client: process.env.DB_CLIENT,
     connection: {
       user: process.env.DB_USER,
-      port: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
       host: process.env.DB_HOST,
       database: process.env.DB_NAME,
       password: process.env.DB_PASSWORD,
