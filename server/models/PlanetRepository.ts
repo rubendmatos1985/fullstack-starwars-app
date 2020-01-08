ddeb95a0685bf6498136c94aacc67d536397395c
ruben1985 <rubendmatos1985@gmail.com>
2020-01-08 11:03:21 +0100

    planet conext used by planet repository

--- server/models/PlanetRepository.ts
+++ server/models/PlanetRepository.ts
@@ -1,42 +1,7 @@
-import { knex } from "../DB";
-import { IPlanetViewModel } from "./ViewModels/PlanetViewModel";
+import { PlanetContext } from "../DB/PlanetContext";
 
 export default (function () {
-  const get = (field?: string) =>
-    function (value?: string): Promise<IPlanetViewModel[]> {
-      const k = field && value
-        ? field === 'name'
-          ? knex.where(field, 'like', `%${value}%`)
-          : knex.where(field, value)
-        : knex
-      return k
-        .select('planet.*', 'residents.json_agg as residents', 'films.json_agg as films')
-        .from(
-          function () {
-            this
-              .select('planet.id as planet_id', knex.raw('json_agg(people.name)'))
-              .from('planet')
-              .leftJoin('resident', 'resident.planet_id', 'planet.id')
-              .leftJoin('people', 'people.id', 'resident.people_id')
-              .groupBy('planet.id')
-              .as('residents')
-          }
-        )
-        .join(
-          function () {
-            this
-              .select('planet.id as planet_id', knex.raw('json_agg(film.title)'))
-              .from('planet')
-              .leftJoin('planets_in_films', 'planets_in_films.planet_id', 'planet.id')
-              .leftJoin('film', 'planets_in_films.film_id', 'film.id')
-              .groupBy('planet.id')
-              .as('films')
-          },
-          'residents.planet_id',
-          'films.planet_id'
-        )
-        .join('planet', 'planet.id', 'residents.planet_id')
-    }
+  const {get} = PlanetContext 
   return {
     getById: get('id'),
 
