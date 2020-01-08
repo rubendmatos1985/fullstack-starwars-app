600a28580a49eb3225b79219f95c215b7d6b9237
ruben1985 <rubendmatos1985@gmail.com>
2020-01-08 15:44:19 +0100

    StarshipContext get method ready

--- server/DB/StarshipContext.ts
+++ server/DB/StarshipContext.ts
@@ -1,9 +1,60 @@
 import { IDBContext, knex } from ".";
 import { IStarshipViewModel } from "../models/ViewModels/StarshipViewModel";
+import { ManyToManyTable, EntityTable } from "../types/Tables";
+import { StarshipPilotFieldsNames } from "../types/interfaces/StarshipPilot";
+import { StarshipEntityFields } from "../models/Starship";
+import { StarshipsInFilmsFields } from "../types/interfaces/StarshipsInFilms";
 
-export const StarshipContext:IDBContext<IStarshipViewModel> = 
-({
-    get: ()=> function(){
-        return knex('')
+export const StarshipContext: IDBContext<IStarshipViewModel> =
+  ({
+    get: (field?: string) => function (value?: string) {
+      const k = field && value
+        ? field === 'name'
+          ? knex.where(field, 'like', `%${ value }%`)
+          : knex.where(field, value)
+        : knex
+      return k
+        .select(
+          'starship.id as starship_id',
+          'pilots.json_agg as pilots',
+          'films.json_agg as films'
+        )
+        .from(
+          function () {
+            this
+              .select('starship.id as starship_id', knex.raw('json_agg(people.name)'))
+              .from(EntityTable.Starship)
+              .leftJoin(
+                ManyToManyTable.StarshipPilot,
+                'starship_pilot.starship_id',
+                'starship.id'
+              )
+              .leftJoin(
+                EntityTable.People,
+                'people.id',
+                'starship_pilot.people_id'
+              )
+              .groupBy('starship.id')
+              .as('pilots')
+          }
+        )
+        .join(
+          function () {
+            this
+              .select('starship.id as starship_id', knex.raw('json_agg(film.name)'))
+              .from(EntityTable.Starship)
+              .leftJoin('starships_in_films', 'starships_in_films.starship_id', 'starship.id')
+              .leftJoin('film', 'film.id', 'starships_in_films.film_id')
+              .groupBy('starship.id')
+              .as('films')
+          },
+          'pilots.starship_id',
+          'films.starship_id'
+        )
+        .join(
+          EntityTable.Starship,
+          'starship.id',
+          'pilots.starship_id'
+        )
     }
-})
+  })
