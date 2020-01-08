ba11efe9c4485e73093cbde0271575d292fecfa5
ruben1985 <rubendmatos1985@gmail.com>
2020-01-08 13:38:02 +0100

    specie context working

--- server/DB/SpecieContext.ts
+++ server/DB/SpecieContext.ts
@@ -5,6 +5,35 @@ export const SpecieContext: IDBContext<ISpecieViewModel> =
   ({
     get: (field: string) =>
       function (value: string) {
-        return knex.select('*')
+        return knex.select(
+          'specie.*',
+          'in_race_people.json_agg as people',
+          'films.json_agg as films'
+        )
+          .from(
+            function () {
+              this
+                .select('specie.id as specie_id', knex.raw('json_agg(people.name)'))
+                .from('specie')
+                .leftJoin('race', 'race.specie_id', 'specie.id')
+                .leftJoin('people', 'people.id', 'race.people_id')
+                .groupBy('specie.id')
+                .as('in_race_people')
+              }
+          )
+          .join(
+              function(){
+                this
+                  .select('specie.id as specie_id', knex.raw('json_agg(film.title)'))
+                  .from('specie')
+                  .leftJoin('species_in_films', 'species_in_films.specie_id', 'specie.id')
+                  .leftJoin('film', 'film.id', 'species_in_films.film_id')
+                  .groupBy('specie.id')
+                  .as('films')
+              },
+              'films.specie_id',
+              'in_race_people.specie_id'
+          )
+          .join('specie', 'specie.id', 'in_race_people.specie_id')
       }
   })
--- server/models/SpecieRepository.ts
+++ server/models/SpecieRepository.ts
@@ -1,36 +1,10 @@
-import { getByIdQuery } from "../DB/getById";
-import { asyncMemoize as Mem } from "../utils/memoize";
-import { ISpecieViewModel } from "./ViewModels/SpecieViewModel";
-import { EntityTable, ManyToManyTable, OneToManyTable } from "../types/Tables";
-import { PeopleEntityFields } from "./People";
-import { SpeciesInFilmsFieldsName } from '../types/interfaces/SpeciesInFilms';
-import { RaceFields } from "../types/interfaces/Races";
-import { FilmFieldsNames } from "./Film";
-import { getAll } from "../DB/getAll";
+
+import { SpecieContext } from "../DB/SpecieContext";
 
 export default (() => {
-  const _getById = Mem(getByIdQuery<EntityTable.Specie, ISpecieViewModel>(
-    EntityTable.Specie, [
-    {
-      tableName: EntityTable.Film,
-      showFields: [FilmFieldsNames.Id, FilmFieldsNames.Title],
-      fieldNameInResponse: 'films',
-      manyToManyTableName: ManyToManyTable.SpeciesInFilms,
-      intersectEntityOn: SpeciesInFilmsFieldsName.FilmId,
-      where: SpeciesInFilmsFieldsName.SpecieId
-    },
-    {
-      tableName: EntityTable.People,
-      showFields: [PeopleEntityFields.Id, PeopleEntityFields.Name],
-      fieldNameInResponse: 'people',
-      manyToManyTableName: OneToManyTable.Race,
-      intersectEntityOn: RaceFields.PeopleId,
-      where: RaceFields.SpecieId
-    }
-
-  ]))
+  const { get } = SpecieContext;
   return {
-    getById: _getById,
-    getAll: getAll(EntityTable.Specie, _getById)
+    getById: get('id'),
+    getAll: get()
   }
 })()
