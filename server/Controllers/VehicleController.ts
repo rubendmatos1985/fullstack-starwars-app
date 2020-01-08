02792257f2be720910e1929a13e5f1b3ad644d83
ruben1985 <rubendmatos1985@gmail.com>
2020-01-08 16:37:47 +0100

    try catch in vehicle controllers

--- server/Controllers/VehicleController.ts
+++ server/Controllers/VehicleController.ts
@@ -1,31 +1,59 @@
 import { Router, Response, Request } from 'express';
-import Vehicle  from '../models/VehicleRepository';
+import Vehicle from '../models/VehicleRepository';
 import { IController } from './Controller';
+import { Status } from '../middlewares/helpers';
 
-class VehicleController implements IController{
-    Router: ()=> Router   
-    Pathname: string;
+const failedMessage = {
+  status: Status.Error,
+  message: "We are having problems please try later"
+}
 
-    constructor(){
-        this.Pathname = "vehicles";
+class VehicleController implements IController {
+  Router: () => Router
+  Pathname: string;
 
-        this.Router = ()=>{
-            const router = Router();
-            router.get("/", this.GetAll);
-            router.get("/:id", this.GetById);
-            return router;
-        }
+  constructor() {
+    this.Pathname = "vehicles";
+
+    this.Router = () => {
+      const router = Router();
+      router.get("/", this.GetAll);
+      router.get("/:id", this.GetById);
+      return router;
+    }
+  }
+
+  public async GetById(req: Request, res: Response): Promise<Response> {
+    try {
+      const r = await Vehicle.getById(req.params.id);
+      return res.json(r);
+    } catch (e) {
+      console.log(e)
+      return res.json(failedMessage)
+    }
+  }
+
+
+  public async GetAll(req: Request, res: Response): Promise<Response> {
+    try {
+      const r = await Vehicle.getAll();
+      return res.json(r);
+    } catch (e) {
+      console.log(e)
+      return res.json(failedMessage)
+    }
+  }
+
+  public async GetByName(req: Request, res: Response): Promise<Response> {
+    try {
+      const r = await Vehicle.getByName(req.query.name)
+      return res.json(r)
+    } catch (e) {
+      console.log(e)
+      return res.json(failedMessage)
     }
 
-    public async GetById(req: Request, res: Response): Promise<Response>{
-        const r = await Vehicle.getById(req.params.id);
-        return res.json(r);
-    }
-
-    public async GetAll(req: Request, res: Response): Promise<Response>{
-        const r = await Vehicle.getAll();
-        return res.json(r);
-    }
+  }
 
 }
 
