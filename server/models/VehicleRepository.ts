acf614516bf59d2ccb1d774fdbcbf7bab79c49c6
ruben1985 <rubendmatos1985@gmail.com>
2020-01-08 16:28:03 +0100

    vehicle context implemented by vehicle repository

--- server/models/VehicleRepository.ts
+++ server/models/VehicleRepository.ts
@@ -1,39 +1,9 @@
-import { asyncMemoize as Mem } from "../utils/memoize"
-import { getByIdQuery } from "../DB/getById"
-import { IVehicleViewModel } from "./ViewModels/VehicleViewModel"
-import { EntityTable, ManyToManyTable } from "../types/Tables"
-import { PeopleEntityFields } from "./People"
-import { PilotFieldNames } from "../types/interfaces/Pilot"
-import { FilmFieldsNames } from "./Film"
-import { VehiclesInFilmsFieldsNames } from "../types/interfaces/VehiclesInFilms"
-import { getAll } from "../DB/getAll"
+import { VehicleContext } from "../DB/VehicleContext"
 
 export default (() => {
-  const _getById = Mem(getByIdQuery<EntityTable.Vehicle, IVehicleViewModel>(
-    EntityTable.Vehicle,
-    [
-      {
-        tableName: EntityTable.People,
-        showFields: [PeopleEntityFields.Id, PeopleEntityFields.Name],
-        fieldNameInResponse: 'pilots',
-        manyToManyTableName: ManyToManyTable.Pilot,
-        intersectEntityOn: PilotFieldNames.PeopleId,
-        where: PilotFieldNames.VehicleId
-      },
-      {
-        tableName: EntityTable.Film,
-        showFields: [FilmFieldsNames.Id, FilmFieldsNames.Title],
-        fieldNameInResponse: 'films',
-        manyToManyTableName: ManyToManyTable.VehiclesInFilms,
-        intersectEntityOn: VehiclesInFilmsFieldsNames.FilmId,
-        where: VehiclesInFilmsFieldsNames.VehicleId
-      }
-
-    ]
-  ))
-
+  const { get }  = VehicleContext
   return {
-    getById: _getById,
-    getAll: getAll(EntityTable.Vehicle, _getById)
+    getById: get('id'),
+    getAll: get()
   }
 })()
