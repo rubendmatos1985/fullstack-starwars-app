75c1a5ccb3574b14f5453f30d1910e5e5f44f4de
ruben1985 <rubendmatos1985@gmail.com>
2020-01-14 16:20:23 +0100

    home tests ready

---
+++ spec/01_home.test.ts
@@ -0,0 +1,29 @@
+import request from 'supertest';
+import App from '../server/app';
+import { buildHomeData } from '../server/Controllers/HomeController';
+import { Request } from 'express';
+describe('Home', () => {
+  test('GET /home', async () => {
+    const response = await request(App).get('/home');
+    expect(response.status).toBe(200);
+  });
+  test('redirect from / -> /home', async () => {
+    const response = await request(App).get('/');
+    expect(response.status).toBe(302);
+    expect(response.header.location).toBe('/home')
+    expect(response.redirect).toBe(true)
+  });
+  
+  test('app return api info object', async ()=>{
+    const response = await request(App).get('/home');
+    const formattedResponse = JSON.parse(JSON.stringify(response)); 
+    const homeDataFromResponse = formattedResponse.text;
+    const url = new URL(formattedResponse.req.url);
+    const fakeRequest = {
+      get: (s: string)=> url.host,
+      protocol: url.protocol.split(':')[0]
+    }
+    const homeData =  buildHomeData(fakeRequest as Request)
+    expect(homeDataFromResponse).toBe(JSON.stringify(homeData));
+  })
+});
