2c2d26fdfe551e06692a3cc87b2898a877a32298
ruben1985 <rubendmatos1985@gmail.com>
2020-01-14 16:22:40 +0100

    test refact

--- spec/01_home.test.ts
+++ spec/01_home.test.ts
@@ -10,20 +10,20 @@ describe('Home', () => {
   test('redirect from to /home', async () => {
     const response = await request(App).get('/');
     expect(response.status).toBe(302);
-    expect(response.header.location).toBe('/home')
-    expect(response.redirect).toBe(true)
+    expect(response.header.location).toBe('/home');
+    expect(response.redirect).toBe(true);
   });
-  
-  test('app return api info object', async ()=>{
+
+  test('app return api info object', async () => {
     const response = await request(App).get('/home');
-    const formattedResponse = JSON.parse(JSON.stringify(response)); 
+    const formattedResponse = JSON.parse(JSON.stringify(response));
     const homeDataFromResponse = formattedResponse.text;
     const url = new URL(formattedResponse.req.url);
     const fakeRequest = {
-      get: (s: string)=> url.host,
+      get: (s: string) => url.host,
       protocol: url.protocol.split(':')[0]
-    }
-    const homeData =  buildHomeData(fakeRequest as Request)
+    };
+    const homeData = buildHomeData(fakeRequest as Request);
     expect(homeDataFromResponse).toBe(JSON.stringify(homeData));
-  })
+  });
 });
