7eeba8daa84964be6eca6b809a54762bb8031d37
ruben1985 <rubendmatos1985@gmail.com>
2020-01-16 19:57:59 +0100

    unused files deleted

--- server/utils/evaluate.ts
+++
@@ -1,3 +0,0 @@
-const Maybe = require('folktale/maybe');
-
-export const evaluate = (condition: (arg: any) => boolean, val: any) => condition(val) ? Maybe.Just(val) : Maybe.Nothing();
--- server/utils/logger.ts
+++
@@ -1,7 +0,0 @@
-function logger(p: NodeJS.Process, logFn: (s: string) => void) {
- /* 
-  return (message: string) => p.env.NODE_ENV === 'development' && logFn(message); */
-}
-
-
-export default logger(process, console.log);
