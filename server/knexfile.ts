2e2d36cea56e2691bd9648deba329b23c67514c8
ruben1985 <rubendmatos1985@gmail.com>
2020-01-17 12:20:56 +0100

    knexfile connection to production db fixed

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -2,18 +2,18 @@ import { Config } from 'knex';
 import dotenv from 'dotenv';
 dotenv.config();
 const { DB_USER, DB_PORT, DB_HOST, DB_NAME, DB_PASSWORD, DATABASE_URL } = process.env;
-const connection =
-  process.env.NODE_ENV === 'development' || 'test'
-    ? {
+const connection = (env) =>
+  env === 'production'
+    ? DATABASE_URL + '?ssl=true'
+    : {
         user: DB_USER,
         port: DB_PORT,
         host: DB_HOST,
         database: DB_NAME,
         password: DB_PASSWORD
-      }
-    : DATABASE_URL + '?ssl=true';
+      };
 
 export const config: Config = {
   client: 'pg',
-  connection
+  connection: connection(process.env.NODE_ENV)
 };
