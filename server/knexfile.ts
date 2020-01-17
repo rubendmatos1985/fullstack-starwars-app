5dffff196a298f8d0f3d41304004f53810a3a6cb
ruben1985 <rubendmatos1985@gmail.com>
2020-01-17 12:12:08 +0100

    knex file formatted

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,26 +1,19 @@
-import { Config } from "knex";
+import { Config } from 'knex';
 import dotenv from 'dotenv';
 dotenv.config();
-const {
-  DB_CLIENT,
-  DB_USER,
-  DB_PORT,
-  DB_HOST,
-  DB_NAME,
-  DB_PASSWORD
-} = process.env;
+const { DB_USER, DB_PORT, DB_HOST, DB_NAME, DB_PASSWORD, DATABASE_URL } = process.env;
+const connection =
+  process.env.NODE_ENV === 'development' || 'test'
+    ? {
+        user: DB_USER,
+        port: DB_PORT,
+        host: DB_HOST,
+        database: DB_NAME,
+        password: DB_PASSWORD
+      }
+    : DATABASE_URL + '?ssl=true';
 
-const connection = process.env.NODE_ENV === 'development' || 'test'
-? {
-  user: DB_USER,
-  port: DB_PORT,
-  host: DB_HOST,
-  database: DB_NAME,
-  password: DB_PASSWORD
-}
-: process.env.DATABASE_URL + '?ssl=true'
-
-export const config:Config = {
+export const config: Config = {
   client: 'pg',
-  connection,
-}
+  connection
+};
